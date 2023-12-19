a0ab5f05b8542d13d10da5142cb832a35f665f61
zephyCode <utkarsh.pundeer2001@outlook.com>
2023-12-19 19:52:24 +0530

    file upload added

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
     return render_template("index.html")
+
+@app.route('/result', methods=['GET','POST'])
+def result():
+    return render_template('result.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
